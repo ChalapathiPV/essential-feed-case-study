35eb79bc07890007f8bdc90cc43d9c0cae070bdd
Chalapathi <38700758+ChalapathiPV@users.noreply.github.com>
2023-03-01 23:18:25 +0000

    Create the protocol FeelLoaderBoundry

---
+++ EssentialFeed/FeedLoader.swift
@@ -0,0 +1,17 @@
+//
+//  FeedLoader.swift
+//  EssentialFeed
+//
+//  Created by PVC on 01/03/23.
+//
+
+import Foundation
+
+enum LoadFeedResult  {
+    case success([FeedItem])
+    case error(Error)
+}
+
+protocol FeedLoader {
+    func load(completion: @escaping (LoadFeedResult) -> Void)
+}
