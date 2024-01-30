5d7d16a9bc46a8e5169edb877be557f8ed941233
Chalapathi <38700758+ChalapathiPV@users.noreply.github.com>
2024-01-30 14:41:36 +0000

    Deleting an already empty cache has no side-effects (cache stays empty and does not fail

--- EssentialFeed/EssentialFeedTests/FeedCache/CodableFeedStoreTests.swift
+++ EssentialFeed/EssentialFeedTests/FeedCache/CodableFeedStoreTests.swift
@@ -54,10 +54,18 @@ class CodableFeedStore {
     }
 
     func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping FeedStore.InsertionCompletion) {
-        let encoder = JSONEncoder()
-        let cache = Cache(feed: feed.map(CodableFeedImage.init), timestamp: timestamp)
-        let encoded = try! encoder.encode(cache)
-        try! encoded.write(to: storeURL)
+        do {
+            let encoder = JSONEncoder()
+            let cache = Cache(feed: feed.map(CodableFeedImage.init), timestamp: timestamp)
+            let encoded = try encoder.encode(cache)
+            try encoded.write(to: storeURL)
+            completion(nil)
+        } catch {
+            completion(error)
+        }
+    }
+    
+    func deleteCachedFeed(completion: @escaping FeedStore.DeletionCompletion) {
         completion(nil)
     }
 }
@@ -138,6 +146,31 @@ final class CodableFeedStoreTests: XCTestCase {
         
         XCTAssertNil(latestInsertionError, "Expected to override cache successfully")
         expect(sut, toRetrieve: .found(feed: latestFeed, timestamp: latestTimestamp))
+    }
+    
+//    func test_insert_deliversErrorOnInsertionError() {
+//        let invalidStoreURL = URL(string: "invalid://store-url")!
+//        let sut = makeSUT(storeURL: invalidStoreURL)
+//        let feed = uniqueImageFeed().local
+//        let timestamp = Date()
+//
+//        let insertionError = insert((feed, timestamp), to: sut)
+//
+//        XCTAssertNotNil(insertionError, "Expected cache insertion to fail with an error")
+//        expect(sut, toRetrieve: .empty)
+//    }
+    
+    func test_delete_hasNoSideEffectsOnEmptyCache() {
+        let sut = makeSUT()
+        let exp = expectation(description: "Wait for cache deletion")
+
+        sut.deleteCachedFeed { deletionError in
+            XCTAssertNil(deletionError, "Expected empty cache deletion to succeed")
+            exp.fulfill()
+        }
+        wait(for: [exp], timeout: 1.0)
+
+        expect(sut, toRetrieve: .empty)
     }
     
     // - MARK: Helpers
