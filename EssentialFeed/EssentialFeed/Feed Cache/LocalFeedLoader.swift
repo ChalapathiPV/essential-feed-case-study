0f46ae0eee614bb36777a81cbbd7cb69a8d1a476
Chalapathi <38700758+ChalapathiPV@users.noreply.github.com>
2024-05-08 10:01:36 +0100

    Nest LoadFeedResult into the FeedLoader protocol as `FeedLoader.Result` since they're closely related

--- EssentialFeed/EssentialFeed/Feed Cache/LocalFeedLoader.swift
+++ EssentialFeed/EssentialFeed/Feed Cache/LocalFeedLoader.swift
@@ -41,8 +41,8 @@ extension LocalFeedLoader {
 }
 
 extension LocalFeedLoader: FeedLoader {
-    public typealias LoadResult = LoadFeedResult
-    public func load(completion: @escaping (LoadFeedResult) -> Void) {
+    public typealias LoadResult = FeedLoader.Result
+    public func load(completion: @escaping (FeedLoader.Result) -> Void) {
         store.retrieve { [weak self] result in
             guard let self = self else { return }
             
