67acae3595d5e398628368fb1c26e9552d8855b9
Chalapathi <38700758+ChalapathiPV@users.noreply.github.com>
2025-09-05 20:03:54 +0100

    Add localized FeedPresenter.title

--- EssentialFeed/EssentialFeedTests/Feed Presentation/FeedPresenterTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Presentation/FeedPresenterTests.swift
@@ -58,6 +58,15 @@ final class FeedPresenter {
         self.errorView = errorView
     }
     
+    static var title: String {
+        return NSLocalizedString(
+           "FEED_VIEW_TITLE",
+           tableName: "Feed",
+           bundle: Bundle(for: FeedPresenter.self),
+           comment: "Title for the feed view"
+        )
+    }
+    
     func didStartLoadingFeed() {
         errorView.display(.noError)
         loadingView.display(FeedLoadingViewModel(isLoading: true))
@@ -77,6 +86,10 @@ final class FeedPresenter {
 }
 
 class FeedPresenterTests: XCTestCase {
+    
+    func test_title_isLoacalized() {
+        XCTAssertEqual(FeedPresenter.title, localized("FEED_VIEW_TITLE"))
+    }
 
     func test_init_doesNotSendMessagesToView() {
         let (_, view) = makeSUT()
